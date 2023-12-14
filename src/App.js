4bf0d7a5a66c6156908dd5901bd18ad96e6e3af2
TranTuanCG <tuantn.work@gmail.com>
2023-12-15 00:51:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [images, setImages] = useState(null);
+    const [value, setValue] = useState(null);
+    const getImages = async () => {
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    message: value
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            }
+            const response = await fetch('http://localhost:8000/images', options);
+            const data = await response.json();
+            console.log(data);
+            setImages(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    console.log(value);
+
+    return (
+        <div className="app">
+            <section className="search-section">
+                <p>
+                    <span className="surprise">TẠO ẢNH BẰNG AI</span>
+                </p>
+                <div className="input-container">
+                    <input
+                        value={value}
+                        placeholder="Nhập nội dung..."
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button onClick={getImages}>Generate</button>
+                </div>
+            </section>
+            <section className="image-section">
+                {images?.map((images, _index) => (
+                    <img key={_index} src={images.url} alt={`Generated img of ${value}`}/>
+                ))}
+            </section>
+        </div>
+    );
 }
 
 export default App;
